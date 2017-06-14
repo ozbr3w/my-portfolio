b30d6e870bb512f059174b78f7a034b590b481a2
Bruce Kyle <brucewkyle@gmail.com>
2017-06-14 14:38:46 +1000

    testing php

---
+++ test.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Site - Index</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Nunito" rel="stylesheet">
+  </head>
+  <body>
+
+<!-- Header Nav -->
+<div class="row">
+	<div class="column">
+		<div class="header-nav">
+			<img src="img/batman-logo.svg" alt="logo" width="150" height="71">
+			<p class="header-nav-subtitle">Designer and Illustrator</p>
+			<div class="menu-centered">
+				<ul class="menu">
+					<li><a href="index.html" class="header-nav-link active">Portfolio</a></li>
+					<li><a href="about.html" class="header-nav-link">About</a></li>
+					<li><a href="contact.html" class="header-nav-link">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php echo "test"; ?>
+
+<!-- Footer -->
+<div class="row">
+	<div class="column">
+		<div class="menu-centered">
+			<ul class="menu">
+				<li><a href="#"><img src="img/pinterest.png" width="50" alt=""></a></li>
+				<li><a href="#"><img src="img/linkedin.png" width="50" alt=""></a></li>
+				<li><a href="#"><img src="img/behance.png" width="50" alt=""></a></li>
+				<li><a href="#"><img src="img/codepen.png" width="50" alt=""></a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
